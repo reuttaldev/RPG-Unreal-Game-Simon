dfa36d3354cfc78f62ae4af46aca3609944a6ebb
reutmgc <reutgaming@gmail.com>
2024-09-08 11:41:06 +0300

    fixing animation paper- forgot to connect on game init in the event graph of the animation to set "as actor". Added simon png

---
+++ Source/CplusProject/PlayerMovement.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlayerMovement.h"
+
+PlayerMovement::PlayerMovement()
+{
+}
+
+PlayerMovement::~PlayerMovement()
+{
+}
